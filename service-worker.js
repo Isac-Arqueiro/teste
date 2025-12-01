979621dd815fd181fb8bd344b255270261afc4b8
Isac-Arqueiro <84213825+Isac-Arqueiro@users.noreply.github.com>
2025-12-01 14:21:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,9 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado.");
+self.addEventListener("install", () => {
+  console.log("SW instalado");
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker ativado.");
+self.addEventListener("activate", () => {
+  console.log("SW ativado");
   return self.clients.claim();
-});
-
-// CACHE BÁSICO → opcional, mas melhora desempenho
-const CACHE_NAME = "pwa-cache-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
 });
